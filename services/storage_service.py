ea70583fcc6978158e1e04e2d78ed79ebe054ab3
RiyaTechoon <riyas@techoon.in>
2026-01-08 15:15:17 +0530

    Update storage_service.py

--- services/storage_service.py
+++ services/storage_service.py
@@ -2,6 +2,12 @@
 
 students = []
 
+def phone_exists(phone):
+    for s in students:
+        if s.phone == phone:
+            return True
+    return False
+
 def add_student(student):
     students.append(student)
 
